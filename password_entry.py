65962ccd0fbd01f3f91428277dadf8e9ecddb5db
ram-asokan93 <53206123+ram-asokan93@users.noreply.github.com>
2019-08-13 14:01:14 +0800

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,4 +1,17 @@
 """
 Ramkumar Asokan
 """
+MIN_LENGTH = 4
+MAX_LENGTH = 9
 
+print("Enter a password: ")
+print("\t Needs to be between {} and {} characters long".format(MIN_LENGTH, MAX_LENGTH))
+password = input(">")
+
+while len(password) > MAX_LENGTH or len(password) < MIN_LENGTH:
+    print("Enter a valid password: Between {} and {} characters".format(MIN_LENGTH, MAX_LENGTH))
+    password = input("\t>")
+
+
+print('Password accepted:')
+print('*'*(len(password)))
